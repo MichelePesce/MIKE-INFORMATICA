a1ff0db4c95b8ff1adcf8bd82cffba28a6a009d5
MichelePesce <124156697+MichelePesce@users.noreply.github.com>
2023-02-01 13:31:31 +0100

    dfbg

---
+++ SenzaTitolo2.cpp
@@ -0,0 +1,40 @@
+/*
+    Name:michele pesce
+    Copyright:colamonico charulli
+    Author:michele pesce
+    Date: 28/09/22 09:39
+    Description:Questo programma, dati in input il prezzo da pagare e la banconota a mia disposizione, mi dice quante e quali monete o banconote dovrò restituire
+*/
+
+
+//1. includo le librerie
+#include <iostream>
+using namespace std;
+
+
+//2. inizio il blocco main
+int main()
+{
+
+//3. dichiaro e inizializzo le variabili
+float mon1 =0;
+floa mon2 =0;
+float mon3=0;
+float mon4 =0;
+float mon5 =0;
+float mon6 =0;
+float mon7 =0;
+float mon8 =0;
+//4. input
+
+//5. logica - operazioni - algoritmo
+
+//6. output
+
+
+//7. ritorno al sistema operativo
+return 0;
+
+//8. fine del programma
+	
+}
